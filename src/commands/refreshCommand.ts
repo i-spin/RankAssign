f1ec9488a7701e30154eeafb2c90bc73e0574d49
sourtaste000 <me@sourtaste000.dev>
2022-02-23 13:44:24 -0800

    refresh for this guild

--- src/commands/refreshCommand.ts
+++ src/commands/refreshCommand.ts
@@ -12,7 +12,30 @@ const config = {
   cooldown: ms('10m'),
 };
 
-const invoke = (message: Message, _args: string[]) => {
+const invoke = (message: Message, args: string[]) => {
+  if (args[0] === 'guild') {
+    if (!message.member?.permissions.has('MANAGE_ROLES')) {
+      message.channel.send('You don\'t have permission to do that.');
+      return;
+    }
+
+    database.get().guild
+      .find((guild) => guild.id === message.guild?.id)?.users
+      .forEach(async (user) => {
+        const tetrioUser = database.getUserById(user.discord.handle);
+        if (!tetrioUser) {
+          message.reply(`Could not find user ${user.discord.handle} in database.`);
+          return;
+        }
+        const result: TetraUser = await (await fetch(`https://ch.tetr.io/api/users/${tetrioUser.tetrio.handle}`)).json() as any;
+        if (!result.success) {
+          message.reply(`Failed to update rank for user ${user.discord.handle}`);
+          return;
+        }
+        tetrioUser.tetrio.rank = result.data.user.league.rank;
+      });
+  }
+
   database.get().guild
     .filter((guild) => guild.id === message.guild?.id)
     .flatMap((guild) => guild.users)
@@ -20,9 +43,14 @@ const invoke = (message: Message, _args: string[]) => {
     .flatMap((user) => user.tetrio)
     .map(async (tetrio) => {
       const result: TetraUser = await (await fetch(`https://ch.tetr.io/api/users/${tetrio.handle}`)).json() as any;
+      if (!result.success) {
+        message.reply(`Failed to update rank for user ${tetrio.handle}`);
+        return tetrio.rank;
+      }
       message.channel.send(`Your rank is now ${database.getUserById(message.author.id)?.tetrio.rank}`);
       return result.data.user.league.rank.toUpperCase();
     });
+  database.save();
 };
 
 export {
