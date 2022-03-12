8fc3596939e48852a1cbf28d3fadf4896fd3f32f
sourtaste000 <sourtaste000@i-sp.in>
2022-03-12 12:51:10 -0800

    omit bad and duplicate commands

--- src/controllers/commandController.ts
+++ src/controllers/commandController.ts
@@ -9,7 +9,19 @@ for await (const command of Deno.readDir(path.resolve("src", "commands"))) {
     const commandModule = await import(
       path.resolve("src", "commands", `${commandName}.ts`)
     );
+
+    if (commands.filter((cmd) => cmd.config.name === commandName).length > 0) {
+      console.log(`Command ${commandName} already exists, skipping.`);
+      continue;
+    }
+
+    if (!commandModule.config || !commandModule.invoke) {
+      console.log(`Command ${commandName} is empty, skipping.`);
+      continue;
+    }
+
     commands.push(commandModule);
+    console.log(`Loaded command ${commandName}`);
   }
 }
 
